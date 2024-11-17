2a002d494368d5f1d9db61092424f27aaad217af
Mayacine Diop <diopmayacine86@gmail.com>
2024-11-17 12:43:27 +0100

    fix test on room controller

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   # associations
-  has_many :votes
-  has_many :contributors
+  has_many :votes, dependent: :destroy
+  has_many :contributors, dependent: :destroy
 
   def status_created?
     status == 1
